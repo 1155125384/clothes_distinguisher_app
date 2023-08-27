fd726a0fc8a0c3a4acffb1eb09cfcb0c83d1ca60
samuelfongky <samuelfong5005@gmail.com>
2023-08-27 10:19:05 +0800

    first commit

---
+++ js/set_theme.js
@@ -0,0 +1,11 @@
+function setLightTheme() {
+    var sidebar = document.querySelector('.sidebar');
+    sidebar.style.backgroundColor = '#fff';
+    var sidebar_txt = document.querySelector('.logo_name');
+    sidebar_txt.style.color = '#c85a5a';
+}
+
+function setDarkTheme() {
+    var sidebar = document.querySelector('.sidebar');
+    sidebar.style.backgroundColor = '#333';
+}
